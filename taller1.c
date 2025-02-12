fc8ee04d18cd8825649dda69e5b324327c705800
GerardoRopero <141593035+GerardoRopero@users.noreply.github.com>
2025-02-12 08:10:05 -0500

    Update taller1.c

--- taller1.c
+++ taller1.c
@@ -2,35 +2,35 @@
 #include <stdbool.h>
 #include <math.h>
 
-int factorialRecursivo(int n);
-int factorialIterativo(int n);
+long factorialRecursivo(int n);
+long factorialIterativo(int n);
 bool esPrimo(int n);
 void conjuntoPrimosIntervalo(int a, int b);
 int algoritmoEuclides(int a, int b);
 
 int main() {
-    printf("%d", factorialRecursivo(5));
+    printf("%ld", factorialRecursivo(34));
     printf("\n");
-    printf("%d", factorialIterativo(5));
+    printf("%ld", factorialIterativo(5));
     printf("\n");
-    printf("%s", esPrimo(5) ? "Sí" : "No");
+    printf("%s", esPrimo(10) ? "Sí" : "No");
     printf("\n");
     
-    conjuntoPrimosIntervalo(1, 100);
+    conjuntoPrimosIntervalo(1, 10);
 
-    printf("%d", algoritmoEuclides(10, 100));
+    printf("%d", algoritmoEuclides(10, 15));
 
     return 0;
 }
 
-int factorialRecursivo(int n) {
+long factorialRecursivo(int n) {
     if (n == 0 || n == 1) {
         return 1;
     }
     return n * factorialRecursivo(n - 1);
 }
 
-int factorialIterativo(int n) {
+long factorialIterativo(int n) {
     int resultado = 1;
     for (int i = 1; i <= n; i++) {
         resultado = resultado * i;
